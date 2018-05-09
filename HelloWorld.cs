04d8e4a7349b1901ce06b6af73dcd741238c14d0
kemirdin <qemirdin@gmail.com>
2018-05-09 20:49:10 +0200

    print Hello World Using C#

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+namespace SampleCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.WriteLine("Hello World");
+        }
+    }
+}
