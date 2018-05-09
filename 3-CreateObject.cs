820854efc3a52248746d08095c71e3bad91566f4
kemirdin <qemirdin@gmail.com>
2018-05-09 21:03:31 +0200

    Create Object Using C#

---
+++ 3-CreateObject.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SampleCodes
+{
+  
+    class Test
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Test t = new Test();
+        }
+    }
+}
