a49a8ee5e4c03c59ae3c428f839075f03fec12ec
kemirdin <qemirdin@gmail.com>
2018-05-09 20:57:48 +0200

    create a class Using C#

---
+++ 2-CreateClass.cs
@@ -0,0 +1,23 @@
+using System;
+// we can also create seprate with namespace 
+namaspace MyNameSpace
+{
+    class test{
+
+    }
+}
+namespace SampleCodes
+{
+   // this is create the class inside of namaspace
+    // class test
+    // {
+
+    // }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
