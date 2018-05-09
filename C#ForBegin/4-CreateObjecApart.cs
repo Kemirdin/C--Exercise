39c812de7a07d144ff7346fe5c9867e16c0d3ed6
kemirdin <qemirdin@gmail.com>
2018-05-09 22:47:14 +0200

    Create object sprate Using C#

---
+++ C#ForBegin/4-CreateObjecApart.cs
@@ -0,0 +1,21 @@
+using System;
+using MyNamespace;
+namespace MyNamespace
+{
+    class Test
+    {
+
+    }
+}
+namespace SampleCodes
+{
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Test t = new Test();
+        }
+    }
+}
