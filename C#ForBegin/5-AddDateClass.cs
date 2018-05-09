c45796a21c6b703387178f00e19a8dbf4b7875d4
kemirdin <qemirdin@gmail.com>
2018-05-09 22:52:27 +0200

    Add Data Members To a class  Using C#

---
+++ C#ForBegin/5-AddDateClass.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SampleCodes
+{
+  
+    class Test
+    {
+       public int X;
+       private char C;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Test t = new Test();
+        }
+    }
+}
