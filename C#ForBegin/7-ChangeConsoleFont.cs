b0be574296ae8d6a5211efa4bde8ea50762f8cf9
kemirdin <qemirdin@gmail.com>
2018-05-10 12:42:37 +0200

    Change Console Font Color Using C#

---
+++ C#ForBegin/7-ChangeConsoleFont.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SampleCodes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          console.ForegroundColor = ConsoleColor.Green;
+          console.WriteLine("Selam Dunya");
+        
+          console.ForegroundColor = ConsoleColor.Blue;
+          console.WriteLine("Selam Dunya");
+        }
+    }
+}
