0c4b9d5f76428c605c523cb5fcfb187a3bc10b5d
kemirdin <qemirdin@gmail.com>
2018-05-10 13:06:42 +0200

    Convert int to String Using ToSting() in C#

---
+++ C#ForBegin/9-ConvertString.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+         int x = 10;
+         string str = x.ToString(); 
+          
+      }
+  }  
+}
