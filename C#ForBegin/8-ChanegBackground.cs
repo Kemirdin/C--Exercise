d361eec7742cf45fa63c775b8200a97ce0e6cd07
kemirdin <qemirdin@gmail.com>
2018-05-10 13:00:04 +0200

    Change Console background  Color Using C#

---
+++ C#ForBegin/8-ChanegBackground.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+          console.BackgroundColor = ConsoleColor.Yellow;
+          console.WriteLine("Selam Dunya");
+
+          console.BackgroundColor=ConsoleColor.green;
+          console.WriteLine("Men keldim");
+      }
+  }  
+}
+
