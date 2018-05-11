9e8b8b8cc0084aa1db0c5114d25ebf3e14c4cdc9
kemirdin <qemirdin@gmail.com>
2018-05-11 21:47:32 +0200

    Convert String To DateTime Using Convert.ToDateTime() In C#

---
+++ C#ForBegin/15-ConvertDateTime.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+         string str = "5/11/2018";
+         DateTime dt = Convert.ToDateTime(str);
+      }
+  }  
+}
