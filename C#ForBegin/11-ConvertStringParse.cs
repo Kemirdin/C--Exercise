e81a245467cb7302127603881256d7c6dda6942e
kemirdin <qemirdin@gmail.com>
2018-05-11 20:05:02 +0200

    Convert String to Int Using Convert.ToInt32()in C#

---
+++ C#ForBegin/11-ConvertStringParse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+         string str = "12345";
+         int a = Convert.ToInt32(str)
+      }
+  }  
+}
