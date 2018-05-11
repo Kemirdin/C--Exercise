8d32924c71dda7065766c477fea0f21a7adc0367
kemirdin <qemirdin@gmail.com>
2018-05-11 20:53:46 +0200

    Convert String To Int Using Int.Parse() In C#

---
+++ C#ForBegin/12-ConvertStringParse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+         string str = "12345";
+         int a = int.Parse(str);
+      }
+  }  
+}
