5bfa83aa67b518593a7ac8b9dbbbe2a1f2dc418a
kemirdin <qemirdin@gmail.com>
2018-05-12 19:41:48 +0200

    Read Input From Keyboard Using Console.ReadLinde()

---
+++ C#ForBegin/17-ReadInput.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+        string str = "";
+
+        str = console.ReadLine();
+      }
+  }  
+}
