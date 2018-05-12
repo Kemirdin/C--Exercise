0e46c2707039a613b31bc16d3cb3a21314daf56a
kemirdin <qemirdin@gmail.com>
2018-05-12 19:47:38 +0200

    Read Int Input From Keybosrd Using Console.ReadLine

---
+++ C#ForBegin/18-ReadIntInput.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       int x = Convert.ToInt32(Console.ReadLine());
+      }
+  }  
+}
