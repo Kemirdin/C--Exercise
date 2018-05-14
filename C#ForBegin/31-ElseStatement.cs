b9ac852378c101fab12b2a861707f32955bbdc15
kemirdin <qemirdin@gmail.com>
2018-05-14 15:41:17 +0200

    Choosing between two alternatives

---
+++ C#ForBegin/31-ElseStatement.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       int myAge = 25;
+       int yourAge = 30;
+
+       if (myAge > yourAge){
+       Console.WriteLine ( "You can drive.");
+       }
+       else
+       {
+           
+                 
+          Console.WriteLine("I am older than you!");       
+
+      }
+  }  
+}
