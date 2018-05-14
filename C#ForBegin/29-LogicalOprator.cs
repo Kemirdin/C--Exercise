db4ae8079ef433f67bf3b82875e9296862b90835
kemirdin <qemirdin@gmail.com>
2018-05-14 15:32:14 +0200

    exercise with Logical operator

---
+++ C#ForBegin/29-LogicalOprator.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       int myAge = 25, minAge = 18, maxAge = 80;
+
+       if (myAge >=minAge && myAge <= maxAge )
+       Console.WriteLine ( "You can drive.");
+
+
+      }
+  }  
+}
