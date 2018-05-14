3839a1b84de48c09765ee25bf88d4b5215d24534
kemirdin <qemirdin@gmail.com>
2018-05-14 16:52:32 +0200

    Exercise For loop with Fizzbin

---
+++ C#ForBegin/37-FizBin.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       for (int i = 1; i <= 100; i++)
+       {
+           if (i % 3 == 0 && i % 5 ==0)
+           {
+               Console.WriteLine("FizzBin!");
+           }
+           else if (i % 3 ==0)
+           {
+               Console.WriteLine("Fizz!");
+           }
+           else
+           {
+               Console.WriteLine(i);
+           }
+       }
+
+       }
+
+
+      }
+  }  
+}
