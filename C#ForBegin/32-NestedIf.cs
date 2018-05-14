d135ecb50da4a76923c7d683a4455d7d2742cc9c
kemirdin <qemirdin@gmail.com>
2018-05-14 15:47:55 +0200

    final else condition

---
+++ C#ForBegin/32-NestedIf.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       double temperature =...;
+       if (temperature < 65){
+           Console.WriteLine("Wear a coat.");
+       }
+else if (temperature> 90)
+{
+    Console.WriteLine("Stay inside.");
+}
+else{
+    Console.WriteLine("It is very pleasant outside.");
+}
+
+      }
+  }  
+}
