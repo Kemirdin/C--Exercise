71e372e0b78f99c9b475a1d074fe9e5181b7bb4a
kemirdin <qemirdin@gmail.com>
2018-05-14 16:09:01 +0200

    difference between Write and WriteLine

---
+++ C#ForBegin/35-WriteWriteLine.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       int answer = 2;
+       switch (answer)
+       {
+           Console.Write("Choice(1-3):");
+           string choice = Console.ReadLine();
+           int value = int.parse(choice);
+
+       }
+
+
+      }
+  }  
+}
