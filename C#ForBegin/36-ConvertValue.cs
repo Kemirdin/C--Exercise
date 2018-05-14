aea0751a9ac7137f1856cf80256af86df1c22eb2
kemirdin <qemirdin@gmail.com>
2018-05-14 16:11:54 +0200

    Convert to String

---
+++ C#ForBegin/36-ConvertValue.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       int answer = 2;
+       switch (answer)
+       {
+           double temperature = 65.0;
+           string temperatureText = temperature.ToString();
+           Console.WriteLine("It is {0} degrees ",temperatureText);
+
+       }
+
+
+      }
+  }  
+}
