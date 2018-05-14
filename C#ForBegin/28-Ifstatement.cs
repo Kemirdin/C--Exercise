9ce2ba5aeb675679f4ddd4d8a0ce25900ff471e4
kemirdin <qemirdin@gmail.com>
2018-05-14 15:28:48 +0200

    exercise if statement using C#

---
+++ C#ForBegin/28-Ifstatement.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+    
+    int yourAge = 30 ;
+    const int minimumDriverAge = 18;
+
+    if (yourAge >= minimumDriverAge)
+    Console.WriteLine("you can drive!");
+      }
+  }  
+}
