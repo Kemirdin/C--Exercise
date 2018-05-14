b846cb1ff9986eb0a537a12a61973afe0ee8c48e
kemirdin <qemirdin@gmail.com>
2018-05-14 15:38:12 +0200

    using or logical opratpr

---
+++ C#ForBegin/30-LogicalOrOperator.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+       string weather = ...;
+
+       if (weather=="Cloudy" || weather == "Rainy"){
+       Console.WriteLine ( "Bring an umbrella");
+
+       }
+      }
+  }  
+}
