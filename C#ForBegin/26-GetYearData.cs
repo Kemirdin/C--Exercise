e25c3e1afe863110da50d0cc7727e0347a3a0e46
kemirdin <qemirdin@gmail.com>
2018-05-14 11:42:48 +0200

    How to calculte yourYearn born

---
+++ C#ForBegin/26-GetYearData.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SampleCodes
+{
+  class Program
+
+  {
+      static void  Main(string[] args)
+      {
+          int age = 30;
+          int thisYear = DateTime.Now.Year;
+          int YearBorn = thisYear-age;
+      }
+  }  
+}
