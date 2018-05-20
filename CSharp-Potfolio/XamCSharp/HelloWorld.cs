6c324e82b6de123ae0ad9f32d0ff0acbac22ec48
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-20 22:11:51 +0200

    Hello world Using C sharp

---
+++ CSharp-Potfolio/XamCSharp/HelloWorld.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace HelloWorld {
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("I am studying CSharp!");
+        }
+    }
+}
