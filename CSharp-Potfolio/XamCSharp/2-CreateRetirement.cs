7845e04ec71134cf4e0abd2e20b651f6b326780f
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-20 22:16:59 +0200

    Exercie variable and print create Retirement calculate

---
+++ CSharp-Potfolio/XamCSharp/2-CreateRetirement.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Fundamentals {
+    class MainClass {
+        public static void Main (string[] args) {
+            int myAge = 19;
+            int yourAge = 35;
+
+            const int retirement = 65;
+
+            string message = "Will we be ready to retire soon?";
+
+            myAge = 59; // variable can change its value
+
+            /*
+             * Use subtraction to calculate years-to-retirement
+             */
+            int yearsToRetirementForYou = retirement - yourAge;
+            int yearsToRetirementForMe = retirement - myAge;
+
+            /*
+             * Print the value of a variable
+             */
+            Console.WriteLine (message);
+
+            Console.WriteLine ("Years for you: " + yearsToRetirementForYou); // concatenating strings
+            Console.WriteLine ("Years for me: {0}", yearsToRetirementForMe); // formatting strings
+        }
+    }
+}
