69007336350135985c2d6f93454db27ff93daa1d
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-23 21:51:12 +0200

    blue print of file 8-calculato

---
+++ CSharp-Potfolio/XamCSharp/8-tipper.cs
@@ -0,0 +1,17 @@
+namespace TipCalculation {
+    class Tipper {
+        public double TotalBill;
+
+        public double CalculateTip (double percentage) {
+            return TotalBill * percentage;
+        }
+
+        public double CalculateTip () {
+            return CalculateTip (0.15);
+        }
+
+        public double Divvy (int numberOfDiners) {
+            return TotalBill / numberOfDiners;
+        }
+    }
+}
