eca3a218b7581eb535ada2948b4f3669bc5985c9
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-26 20:30:08 +0200

    main file of jornal using C sharp

---
+++ CSharp-Potfolio/XamCSharp/14-program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyJournal {
+    class Program {
+        static void Main (string[] args) {
+            Journal myJournal = new Journal ();
+            Console.WriteLine (myJournal.JournalMessage);
+        }
+    }
+}
