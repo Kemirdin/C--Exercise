ab6b82e88e77c5fce493af1837b7a6924c57c54f
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-30 13:29:10 +0200

    create another class for backaccount calculation

---
+++ CSharp-Potfolio/XamCSharp/20-BankAccount.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount {
+    public class BankAccount {
+        public double AmountOfMoney {
+            get;
+            private set;
+        }
+
+        public BankAccount (double startingAmount = 500) {
+            AmountOfMoney = startingAmount;
+        }
+
+        public void Desposit (double amount) {
+            AmountOfMoney += amount;
+        }
+
+        public bool Withdraw (double amountToWithdraw) {
+            // Can't overdraft
+            if (AmountOfMoney < amountToWithdraw)
+                return false;
+
+            AmountOfMoney -= amountToWithdraw;
+            return true;
+        }
+    }
+}
