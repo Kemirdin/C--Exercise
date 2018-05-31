26b26980ad281f04858c63c500bf642d6e3a9d0a
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-31 14:49:56 +0200

    create with other class and auto property for use main class

---
+++ CSharp-Potfolio/XamCSharp/22-Employee.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeArray {
+    class Employee {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString () {
+            return string.Format ("{0} {1}", FirstName, LastName);
+        }
+    }
+}
