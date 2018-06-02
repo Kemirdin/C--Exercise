af9d17f12cd046b8533fb58bb1c37bce72b09627
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-02 19:25:10 +0200

    C sharp le yontem 2

---
+++ CSharp-Derisleri/3-String.cs
@@ -0,0 +1,12 @@
+using System;
+namespace SampleCodes {
+    class Program {
+        static void Main (string[] args) {
+            string ogrno = "123";
+            string adi = "Kemirdin";
+            string soyad = "Kaynam";
+            console.Write ("OGRENCI NO :{0} ADI:{1} SOYADI:{2}", ogrno, adi, soyad);
+            console.Readkey ();
+        }
+    }
+}
