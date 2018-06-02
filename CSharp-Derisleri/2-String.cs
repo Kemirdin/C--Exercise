344e4571dacbbddb07544b33cbac286ddb46554b
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-02 19:21:12 +0200

    String yontem 1

---
+++ CSharp-Derisleri/2-String.cs
@@ -0,0 +1,12 @@
+using System;
+namespace SampleCodes {
+    class Program {
+        static void Main (string[] args) {
+            string ogrno = "123";
+            string adi = "Kemirdin";
+            string soyad = "Kaynam";
+            console.Write ("Ogrenci NO:" + ogrno + "" + "ADI:" + adi + "" + "SOYADI:" + soyad);
+            console.Readkey;
+        }
+    }
+}
