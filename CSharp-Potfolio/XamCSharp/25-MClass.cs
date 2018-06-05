e18b648ff495894f03ef5ffca9460077e394f681
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-05 22:24:12 +0200

    another class

---
+++ CSharp-Potfolio/XamCSharp/25-MClass.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman {
+    public class HangmanManager {
+        private string hiddenString;
+
+        public string DisplayString {
+            get;
+            private set;
+        }
+
+        public bool IsGameOver {
+            get {
+                return DisplayString == hiddenString;
+            }
+        }
+
+        public HangmanManager (string hiddenWord) {
+            hiddenString = hiddenWord;
+            CreateDisplayString ();
+        }
+
+        public bool Guess (char guess) {
+            bool foundALetter = false;
+            for (int i = 0; i < hiddenString.Length; i++) {
+                if (hiddenString[i] == guess) {
+                    DisplayString = DisplayString.Remove (i, 1);
+                    DisplayString = DisplayString.Insert (i, guess.ToString ());
+                    foundALetter = true;
+                }
+            }
+            return foundALetter;
+        }
+
+        private void CreateDisplayString () {
+            for (int i = 0; i < hiddenString.Length; i++) {
+                DisplayString += "*";
+            }
+        }
+    }
+}
