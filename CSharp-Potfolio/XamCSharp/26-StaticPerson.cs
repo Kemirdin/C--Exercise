f8a40a0cfa806d1f906560feb121018426d1b5f3
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-07 22:47:23 +0200

    classabout create static structure using c sharp

---
+++ CSharp-Potfolio/XamCSharp/26-StaticPerson.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging {
+    public class Person {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Siblings { get; set; }
+
+        public string HaveBirthday () {
+            return string.Format ("{0} is {1} years old!", Name, ++Age);
+        }
+
+        public string DescribeFamily () {
+            string siblingText;
+
+            if (Siblings == 0) {
+                siblingText = " has no siblings";
+            } else {
+                siblingText = " has " + Siblings + " siblings";
+            }
+
+            return Name + siblingText;
+        }
+
+        public static List<Person> GetPeople () {
+            var people = new List<Person> {
+                new Person { Name = "Rachel", Age = 20, Siblings = 0 },
+                new Person { Name = "Bobby", Age = 16, Siblings = 2 },
+                new Person { Name = "John", Age = 35, Siblings = 1 },
+                new Person { Name = "Mary", Age = 40, Siblings = 4 },
+            };
+
+            return people;
+        }
+    }
+}
