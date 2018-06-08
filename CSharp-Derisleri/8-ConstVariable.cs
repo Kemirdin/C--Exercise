a749719e835b3686286d8bd3f41c4d6c6b11f5e5
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-08 19:36:59 +0200

    use Const Variable

---
+++ CSharp-Derisleri/8-ConstVariable.cs
@@ -0,0 +1,15 @@
+using System;
+namespace SampleCodes {
+    class Program {
+        static void Main (string[] args) {
+            const double pi = 3.14;
+            double yaricap;
+            double alan;
+            Console.Write ("Yaricap degerini giriniz?");
+            yaricap = Convert.ToDouble (Console.ReadLine ());
+            alan = pi * (yaricap * yaricap);
+            Console.Write (alan { 0 }; alan);
+            Console.ReadKey ();
+        }
+    }
+}
