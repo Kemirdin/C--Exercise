b655642d03fb42366dcb050036d3a29b7758492d
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-11 10:29:35 +0200

    create Customer constractor

---
+++ CSharp-Potfolio/CSARP-FUN/3-Customar.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Classes {
+    public class Customer {
+        public int Id;
+        public string Name;
+        public Customer () {
+
+        }
+        public Customer (int Id) {
+            this.Id = id;
+        }
+        public Customer (int Id, string name) {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
+}
