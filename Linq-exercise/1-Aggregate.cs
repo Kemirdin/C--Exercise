e074e9722a54bda730e41ba822cac2e701348dc3
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-12 20:53:36 +0200

    this is about how to create aggregate using liq lamda experession

--- Linq-exercise/1-Aggregate.cs
+++ Linq-exercise/1-Aggregate.cs
@@ -1,6 +1,7 @@
 using System;
+using linq;
 namespace Classes {
-    private static void Sample_Aggregate_Lambda() {
+    private static void Sample_Aggregate_Lambda () {
         var numbers = new int[] { 1, 2, 3, 4, 5 };
 
         var result = numbers.Aggregate ((a, b) => a * b);
