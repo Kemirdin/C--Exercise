93afa0219ed67d252abd7baff607f4645166ac61
Kemirdin Kaynam <qemirdin@gmail.com>
2018-06-13 22:03:22 +0200

    this is about lamda experession with linq

---
+++ Linq-exercise/1-Aggregate2.cs
@@ -0,0 +1,13 @@
+using System;
+using linq;
+namespace Classes {
+    private static void Sample_Aggregate_Lambda () {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+        var result = numbers.Aggregate ((a, b) => a * b);
+
+        Debug.WriteLine ("Aggregated numbers by multiplication:");
+        Debug.WriteLine (result);
+    }
+
+}
