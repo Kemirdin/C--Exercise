0966b16300a5f9039d975a5752919c9a230561af
Kemirdin Kaynam <qemirdin@gmail.com>
2020-06-02 17:11:00 +0200

    How to play varibals

---
+++ C#Fundations/Variables.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharpFundations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var anything = "My String";
+      Console.WriteLine(anything);
+    }
+  }
+}
